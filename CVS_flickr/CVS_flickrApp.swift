8f6fc5a72a4b49617eb5053f6c64f833b0c91939
Tim McEwan <timmcewandev@gmail.com>
2025-03-26 07:27:04 -0600

    Initial Commit

---
+++ CVS_flickr/CVS_flickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVS_flickrApp.swift
+//  CVS_flickr
+//
+//  Created by Tim McEwan on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct CVS_flickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
